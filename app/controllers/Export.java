0d5df031540039ff902b0ba0808a6d0c20235091
Eduard Gamiao <edgamiao@gmail.com>
2014-04-28 11:49:39 -1000

    Added login and admin requirements for export.

--- app/controllers/Export.java
+++ app/controllers/Export.java
@@ -4,6 +4,7 @@ import models.RainGarden;
 import models.RainGardenDB;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security;
 import views.formdata.PlantFormDropdownTypes;
 
 /**
@@ -17,26 +18,31 @@ public class Export extends Controller {
    * Export the rain garden information as a csv.
    * @return The rain garden information as a csv.
    */
+  @Security.Authenticated(Secured.class)
   public static Result exportRainGarden() {
-    // Prepare a chunked text stream
-    Chunks<String> chunks = new StringChunks() {
-      
-      // Called when the stream is ready
-      public void onReady(Chunks.Out<String> out) {
-        rainGardenOutChannel(out);
-      }
-      
-    };
-    
-    // Serves this stream with 200 OK
-    response().setContentType("text/csv");
-    return ok(chunks);
+    if (Secured.isLoggedIn(ctx()) && Secured.getUserInfo(ctx()).isAdmin()) {
+      // Prepare a chunked text stream
+      Chunks<String> chunks = new StringChunks() {
+
+        // Called when the stream is ready
+        public void onReady(Chunks.Out<String> out) {
+          rainGardenOutChannel(out);
+        }
+
+      };
+
+      // Serves this stream with 200 OK
+      response().setContentType("text/csv");
+      return ok(chunks);
+    }
+    return redirect(routes.Application.index());
   }
 
   /**
    * Writes the rain garden information to a stream.
    * @param out The stream being written to.
    */
+  @Security.Authenticated(Secured.class)
   public static void rainGardenOutChannel(Chunks.Out<String> out) {
     out.write("Title,Property_Type,Address,Description,Date_Installed,Rain_Garden_Size,Waterflow_Source_Size,"
         + "Waterflow_Description,Infiltration_Rate,Owner_Name\n");
